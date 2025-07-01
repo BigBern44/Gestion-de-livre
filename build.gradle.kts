748cee4a1dc34cef1f6f36cbe6affc5e7bcf6937
Eliott Bernard <bernard.eliott44@gmail.com>
2025-07-01 13:44:32 +0200

    ajout pitest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("jacoco") // JaCoCo activé ici
-
+	id("jacoco")
+	id("info.solidsoft.pitest") version "1.19.0-rc.1"
 }
 
 group = "com.example"
@@ -42,8 +42,8 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
 	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	testImplementation("org.pitest:pitest-junit5-plugin:1.2.3")
 
 }
 
@@ -69,3 +69,13 @@ tasks.jacocoTestReport {
 		csv.required.set(false)
 	}
 }
+pitest {
+	targetClasses.set(listOf("com.example.demo.domain.*")) // packages à tester
+	targetTests.set(listOf("com.example.demo.*"))
+
+	pitestVersion.set("1.19.6") // version de PIT
+	threads.set(2)
+	outputFormats.set(listOf("HTML"))
+	timestampedReports.set(false)
+}
+
